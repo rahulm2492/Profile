b8ffa2ddc2b21b87af9fff2b63e641bc79e89e31
Rahul Maheshwari <rahul.maheshwari@rbs.com>
2017-05-05 14:14:27 +0530

    Master

--- work.js
+++ work.js
@@ -7,7 +7,7 @@ var pdfDoc = null,
     pageNum = 1,
     pageRendering = false,
     pageNumPending = null,
-    scale =10,
+    scale =1,
     canvas = document.getElementById('the-canvas'),
     ctx = canvas.getContext('2d');
 
